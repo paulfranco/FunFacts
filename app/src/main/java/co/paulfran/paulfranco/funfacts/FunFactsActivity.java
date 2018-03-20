27aad955499cec980c56fe00bea6df4187a67a52
Paul Franco <paulfranco@me.com>
2018-03-20 01:10:44 -0700

    Added Toast

--- app/src/main/java/co/paulfran/paulfranco/funfacts/FunFactsActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/funfacts/FunFactsActivity.java
@@ -2,6 +2,7 @@ package co.paulfran.paulfranco.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -12,6 +13,7 @@ import java.util.Random;
 
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook fackBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare our view variables
@@ -42,6 +44,7 @@ public class FunFactsActivity extends AppCompatActivity {
         };
         showFactButton.setOnClickListener(listener);
 
-        Toast.makeText(this, "yay our activity was created", Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, "yay our activity was created", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "we are logging from the onCreate() method!");
     }
 }
